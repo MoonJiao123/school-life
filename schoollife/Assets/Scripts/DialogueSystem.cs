2f0b65ea7780ff779669caf18520abf410272431
MoonJiao123 <yuj001@ucsd.edu>
2019-10-15 23:37:08 -0700

    dialogue sustem

---
+++ schoollife/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueSystem : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create queue to hold all sentences
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        //animator set open state
+        animator.SetBool("IsOpen", true);
+
+        //initialize name and sentences store in queue
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        //end of sentences finsih conversion
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue() {
+        //set open state to false dialogue box move out of screen
+        animator.SetBool("IsOpen", false);
+    }
+}
---
+++ schoollife/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public void TriggerDialogue() {
+        //find dialogue system object
+        FindObjectOfType<DialogueSystem>().StartDialogue(dialogue);
+    }
+}
