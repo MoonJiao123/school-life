e7409bd2397f95c3d4bdb8279ad5063bf4460a6c
z5ying <z5ying@ucsd.edu>
2019-10-27 22:48:24 -0700

    added UI

---
+++ schoollife/Assets/Scripts/panelBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelBehavior : MonoBehaviour
+{
+    public RectTransform panelRectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Puts the panel to the front as it is now the last UI element to be drawn.
+        panelRectTransform.SetAsLastSibling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
